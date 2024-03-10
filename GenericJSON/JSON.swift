ebf26e8139e4b6c60a39e35f72d0c7753cba507f
Frizlab <fload@me.com>
2024-03-10 16:53:56 +0100

    Fix compilation for iOS, tvOS and watchOS

--- GenericJSON/JSON.swift
+++ GenericJSON/JSON.swift
@@ -65,7 +65,7 @@ extension JSON : CustomDebugStringConvertible {
 				/* TODO: Avoid going through a JSON encoder to create a String representation of the JSON! */
 				let encoder = JSONEncoder()
 				encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-				if #available(macOS 10.15, *) {
+				if #available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *) {
 					encoder.outputFormatting = encoder.outputFormatting.union(.withoutEscapingSlashes)
 				}
 				return try! String(data: encoder.encode(self), encoding: .utf8)!
